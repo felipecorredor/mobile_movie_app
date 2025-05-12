8e61bea3b903158b7148c05c45ba3d84f3408f49
juan.corredor@wizeline.com <juan.corredor@wizeline.com>
2025-05-12 16:21:13 -0500

    Refactor profile and saved tabs to use SafeAreaView; enhance movie details screen with loading state and detailed movie information display

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,13 +1,117 @@
-import { useLocalSearchParams } from "expo-router";
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className="flex-col items-start justify-center mt-5">
+    <Text className="text-light-200 text-sm font-normal">{label}</Text>
+    <Text className="text-light-200 mt-2 font-bold text-sm">
+      {value || "N/A"}
+    </Text>
+  </View>
+);
 
 const Details = () => {
   const { id } = useLocalSearchParams();
 
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
+  if (loading)
+    return (
+      <SafeAreaView className="bg-primary flex-1">
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+
   return (
-    <View>
-      <Text>Movie Details: {id}</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+        </View>
+
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+          <View className="flex-row items-center gap-x-1 mt-2">
+            <Text className="text-light-200 text-sm">
+              {movie?.release_date?.split("-")[0]}
+            </Text>
+            <Text className="text-light-200 text-sm">{movie?.runtime} m</Text>
+          </View>
+
+          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-4" />
+            <Text className="text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)} / 10
+            </Text>
+            <Text className="text-light-200 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
+          </View>
+
+          <MovieInfo label="OverView" value={movie?.overview} />
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${(movie?.budget ?? 0) / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(
+                (movie?.revenue ?? 0) / 1_000_000
+              )} million`}
+            />
+          </View>
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies?.map((c) => c.name).join(" • ") ||
+              "N/A"
+            }
+          />
+        </View>
+      </ScrollView>
+
+      <TouchableOpacity
+        className="absolute bottom-5 right-0 left-0 mx-5 bg-accent rounded-lg py-3.5 flex flex-row items-center justify-center"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor="#ffffff"
+        />
+        <Text className="text-white font-semibold text-base">Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 };
